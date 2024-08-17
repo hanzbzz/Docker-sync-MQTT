d27cafd18bddbcfc49ed963a48c3ae64b2f3d7be
hzbzz <honza.brazda@sezanm.cz>
2024-08-17 13:00:12 +0200

    add basic synchronization with 1 worker and 1 job

--- controller/app.py
+++ controller/app.py
@@ -1,15 +1,33 @@
 import pika
+from functools import partial
+
+workers = ["worker1"]
+
+def response_callback(channel, method_frame, header_frame, body, responses, connection):
+    body = body.decode()
+    print(f"[*] RESPONSE: RECEIVE {body}")
+    responses.append(body)
+    # all workers finished
+    if responses == workers:
+        print("[+] JOB finished")
+        # stop consuming
+        connection.close()
 
 def main():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabitmq-broker'))
     channel = connection.channel()
+    # declare queues
+    channel.queue_declare(queue='command')
+    channel.queue_declare(queue='response')
 
-    channel.queue_declare(queue='hello')
+    # start job1
+    channel.basic_publish(exchange='', routing_key='command', body='job1')
+    print("[+] COMMAND: SEND job1")
 
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
+    responses = []
+    channel.basic_consume(queue='response', on_message_callback=partial(response_callback, responses=responses, connection=connection) , auto_ack=True)
+    channel.start_consuming()
 
 import time
 
--- worker1/app.py
+++ worker1/app.py
@@ -1,17 +1,28 @@
-import pika, sys, os
+import pika
+import random
+import time
 
+def job1():
+    for i in range(5):
+        time.sleep(random.randint(0,2))
+    
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabitmq-broker'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='command')
+    channel.queue_declare(queue='response')
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+        body = body.decode()
+        print(f"[+] COMMAND: RECEIVE {body}")
+        if body == "job1":
+            job1()
+            channel.basic_publish(exchange='', routing_key='response', body='worker1')
+            print("[*] RESPONSE: SEND job1")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='command', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 main()
